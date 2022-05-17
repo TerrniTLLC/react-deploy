f4fe359571a05d8db56021cbaf026064b69e5810
gleb <terrnitllc.work@gmail.com>
2022-05-17 15:39:49 +0500

    deploy react app to gh-pages

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h1>Test deploy react</h1>
+       <h2>It's work </h2>
+       <h2><bold>promise me</bold></h2>
       </header>
     </div>
   );
